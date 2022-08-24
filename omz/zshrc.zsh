8d70f05e6e7f8eabeb0bbfe73f8d4826fa6cd7cd
kcollett <karen.collett@gmail.com>
2022-08-23 20:10:16 -0500

    Initial revision, reflecting modifications made in last week or so.

---
+++ omz/zshrc.zsh
@@ -0,0 +1,170 @@
+#  -*- mode: shell-script; -*-
+#
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+#XXX: Not using P10K for now
+#if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+#  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+#fi
+
+# If you come from bash you might have to change your $PATH.
+# export PATH=$HOME/bin:/usr/local/bin:$PATH
+# I specifically need to do this for emacs shell-mode
+ZSH_DISABLE_COMPFIX="true"
+
+# Path to your oh-my-zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
+# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
+#ZSH_THEME="robbyrussell"
+#ZSH_THEME="minimal"
+ZSH_THEME="agnoster"
+#ZSH_THEME="powerlevel10k/powerlevel10k"
+[ -z ${INSIDE_EMACS} ] && ZSH_THEME="agnoster" || ZSH_THEME="minimal"
+       
+
+# Set list of themes to pick from when loading at random
+# Setting this variable when ZSH_THEME=random will cause zsh to load
+# a theme from this variable instead of looking in $ZSH/themes/
+# If set to an empty array, this variable will have no effect.
+# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+
+# Uncomment the following line to use case-sensitive completion.
+# CASE_SENSITIVE="true"
+
+# Uncomment the following line to use hyphen-insensitive completion.
+# Case-sensitive completion must be off. _ and - will be interchangeable.
+# HYPHEN_INSENSITIVE="true"
+
+# Uncomment one of the following lines to change the auto-update behavior
+# zstyle ':omz:update' mode disabled  # disable automatic updates
+# zstyle ':omz:update' mode auto      # update automatically without asking
+# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+
+# Uncomment the following line to change how often to auto-update (in days).
+# zstyle ':omz:update' frequency 13
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+# DISABLE_MAGIC_FUNCTIONS="true"
+
+# Uncomment the following line to disable colors in ls.
+# DISABLE_LS_COLORS="true"
+
+# Uncomment the following line to disable auto-setting terminal title.
+# DISABLE_AUTO_TITLE="true"
+
+# Uncomment the following line to enable command auto-correction.
+# ENABLE_CORRECTION="true"
+
+# Uncomment the following line to display red dots whilst waiting for completion.
+# You can also set it to another string to have that shown instead of the default red dots.
+# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
+# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
+# COMPLETION_WAITING_DOTS="true"
+
+# Uncomment the following line if you want to disable marking untracked files
+# under VCS as dirty. This makes repository status check for large repositories
+# much, much faster.
+# DISABLE_UNTRACKED_FILES_DIRTY="true"
+
+# Uncomment the following line if you want to change the command execution time
+# stamp shown in the history command output.
+# You can set one of the optional three formats:
+# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# or set a custom format using the strftime function format specifications,
+# see 'man strftime' for details.
+# HIST_STAMPS="mm/dd/yyyy"
+
+# Would you like to use another custom folder than $ZSH/custom?
+# ZSH_CUSTOM=/path/to/new-custom-folder
+
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+#KC: removed vi-mode for now
+#KC: git define a ton of aliases, I prefer to make them as needed
+plugins=(aliases)
+
+source $ZSH/oh-my-zsh.sh
+
+# undo some crap that OMZ has done in omz/lib/directories.zsh
+unsetopt auto_pushd
+unsetopt pushdminus
+unsetopt pushd_ignore_dups
+
+# User configuration
+
+# Not sure if I want to bring in the whole environ/path stuff here,
+# so we'll take baby steps
+PAGER=less
+LESS='--dumb'
+CDPATH="~"
+dots=".[a-zA-Z0-9]*"            # quick way to get at the dot files
+HISTSIZE=500
+#PUSHD_MINUS=1                   # old-school pushd/popd behavior XXX: emacs is new-school
+export PAGER LESS CDPATH dots HISTSIZE
+# suppress agnoster user@host display if logged in as "myself"
+export DEFAULT_USER="karencollett"
+
+
+#
+# PATH setup
+# 
+
+# homebrew (resides in a different directory on Apple Silicon machines)
+brewdir="/usr/local/bin"
+asbrewdir="/opt/homebrew/bin"
+[ -d "$asbrewdir" ] && brewdir="$asbrewdir"
+#echo "brewdir: $brewdir"
+eval "$($brewdir/brew shellenv)"
+#[ -f /opt/homebrew/bin/brew ] && eval "$(/opt/homebrew/bin/brew shellenv)"
+
+
+# typeset -U snarfed from
+# https://unix.stackexchange.com/questions/62579/is-there-a-way-to-add-a-directory-to-my-path-in-zsh-only-if-its-not-already-pre
+typeset -U path PATH
+
+# to prepend, use path=(newdir "$path[@]")
+path+=("$HOME/sh")
+path+=("$HOME/sh.local")
+export PATH
+
+# NB: The important changes are established "brew shellenv" command above
+# export MANPATH="/usr/local/man:$MANPATH"
+# export INFOPATH="/usr/local/INFO:$INFOPATH"
+
+# You may need to manually set your language environment
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# mcfly config
+eval "$(mcfly init zsh)"
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+#XXX: Not using P10K for now
+#[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
