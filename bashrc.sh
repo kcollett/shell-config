2fb916703b1609064ac83f0f4f2c13890860a93a
kcollett <karen.collett@gmail.com>
2022-08-25 20:09:43 -0500

    Remove old setup for cygwin.

--- bashrc.sh
+++ bashrc.sh
@@ -5,9 +5,6 @@
 : For emacs, define environment if it has not been done so already
 [ -z "$ENV_DEFINED" ] && . "$HOME/.environ"
 
-# cygwin
-#shopt -s nocaseglob
-
 # max out file size limit
 #ulimit $(ulimit -H)
 
@@ -19,101 +16,93 @@
 case $- in
 *i*)
 
-set -o emacs
+    set -o emacs
 
-# : we prefer per-shell history
-# unset HISTFILE
+    # : we prefer per-shell history
+    # unset HISTFILE
 
-: set some control variables
-FIGNORE="~"
-HISTCONTROL="ignoredups"		# ignore duplicate commands
-cdable_vars="yes"
-command_oriented_history="yes"
-# this includes . and .. -- no fun for rm *
-#glob_dot_filenames="yes"		# include dot files in '*' expansion
-unset MAILCHECK				# don't check for mail
+    : set some control variables
+    FIGNORE="~"
+    HISTCONTROL="ignoredups" # ignore duplicate commands
+    cdable_vars="yes"
+    command_oriented_history="yes"
+    # this includes . and .. -- no fun for rm *
+    #glob_dot_filenames="yes"		# include dot files in '*' expansion
+    unset MAILCHECK # don't check for mail
 
-: Define common aliases
-source "$HOME/.aliases"
-alias h='history'
+    : Define common aliases
+    source "$HOME/.aliases"
+    alias h='history'
 
-: marking directories
-mark()
-{
-    eval $1=$PWD
-}
+    : marking directories
+    mark() {
+        eval $1=$PWD
+    }
 
-: define some functions
-#source "$FUNDIR/first_choice"
-#source "$FUNDIR/pathfuncs"
-#source "$FUNDIR/xt_title"
-#source "$FUNDIR/gx"
+    : define some functions
+    #source "$FUNDIR/first_choice"
+    #source "$FUNDIR/pathfuncs"
+    #source "$FUNDIR/xt_title"
+    #source "$FUNDIR/gx"
 
-: Alias-of-the-Month setup
-[ -r "$ALIASFILE" ] && . "$ALIASFILE"
+    : Alias-of-the-Month setup
+    [ -r "$ALIASFILE" ] && . "$ALIASFILE"
 
-: define to_tilde function
-#source "$FUNDIR/to_tilde"
+    : define to_tilde function
+    #source "$FUNDIR/to_tilde"
 
-: prompt, emacs shell-mode support
-typeset -x CURDIR
-do_misc ()
-{
-    #CURDIR=$(to_tilde "$PWD")
-    CURDIR="$PWD"
-    CURDIR=$(echo "$CURDIR" | sed "s,..*\(/[^/]*/[^/]*\),...\1,")
-}
-do_misc
+    : prompt, emacs shell-mode support
+    typeset -x CURDIR
+    do_misc() {
+        #CURDIR=$(to_tilde "$PWD")
+        CURDIR="$PWD"
+        CURDIR=$(echo "$CURDIR" | sed "s,..*\(/[^/]*/[^/]*\),...\1,")
+    }
+    do_misc
 
-: Directory stack setup
-#source "$FUNDIR/dirstack.bash"
-#alias cd=chdir
-#cd				# to clean up automounted dreck
+    : Directory stack setup
+    #source "$FUNDIR/dirstack.bash"
+    #alias cd=chdir
+    #cd				# to clean up automounted dreck
 
-formatted_dirs ()
-{
-    declare    fd_tmp=`mktemp /tmp/dirs_XXX`
-    declare -i num=0
- 
-    dirs -p |
-    while read f
-    do
-        echo $num: $f
-	echo "d$num='$f'" >>$fd_tmp
-        let num++
-    done
+    formatted_dirs() {
+        declare fd_tmp=$(mktemp /tmp/dirs_XXX)
+        declare -i num=0
 
-    source "$fd_tmp"
-    rm "$fd_tmp"
-}
+        dirs -p |
+            while read f; do
+                echo $num: $f
+                echo "d$num='$f'" >>$fd_tmp
+                let num++
+            done
 
-push_dir ()
-{
-    [ "$@" ] && pushd "$@" >/dev/null || pushd >/dev/null
-    formatted_dirs
-}
+        source "$fd_tmp"
+        rm "$fd_tmp"
+    }
 
-pop_dir ()
-{
-    [ "$@" ] && popd  "$@" >/dev/null || popd  >/dev/null
-    formatted_dirs
-}
+    push_dir() {
+        [ "$@" ] && pushd "$@" >/dev/null || pushd >/dev/null
+        formatted_dirs
+    }
 
-: Prompt setup
-source "$HOME/.colors"
-#PS1="\h:"'$CURDIR'"$_ids $_time"' $ '
-#PS1="\[${IWhite}\]\h:\W \u\$\[${Color_Off}\] "
-#PS1="\[${IWhite}\]\w\n\$\[${Color_Off}\] "
-# see http://blog.taylormcgann.com/tag/prompt-color/
-export PS1="\[\033[0;35m\]\W\[\033[0m\] \[\033[0;31m\]\u\\[\033[1;37m\]\$\[\033[0m\] "
+    pop_dir() {
+        [ "$@" ] && popd "$@" >/dev/null || popd >/dev/null
+        formatted_dirs
+    }
 
+    : Prompt setup
+    source "$HOME/.colors"
+    #PS1="\h:"'$CURDIR'"$_ids $_time"' $ '
+    #PS1="\[${IWhite}\]\h:\W \u\$\[${Color_Off}\] "
+    #PS1="\[${IWhite}\]\w\n\$\[${Color_Off}\] "
+    # see http://blog.taylormcgann.com/tag/prompt-color/
+    export PS1="\[\033[0;35m\]\W\[\033[0m\] \[\033[0;31m\]\u\\[\033[1;37m\]\$\[\033[0m\] "
 
+    #PS1="\h:\w \# \$ "
+    #PS1='\u@\h \w\n\$ '
 
-#PS1="\h:\w \# \$ "
-#PS1='\u@\h \w\n\$ '
-
-: END case-wrap for interactive shells
-;;
+    : END case-wrap for interactive shells
+    ;;
 esac
 
 : rust setup
